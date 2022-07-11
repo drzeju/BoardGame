c5e5a8bad3814dc71b0dc111f1cf9a912c74ef61
drzeju <jedrzej.gruca@gmail.com>
2022-07-11 15:03:57 +0200

    Removed unused files and import statements from Consumer app

--- RabbitMQConsumer/src/main/java/com/rabbit/rabbitmqconsumer/service/RabbitMQService.java
+++ RabbitMQConsumer/src/main/java/com/rabbit/rabbitmqconsumer/service/RabbitMQService.java
@@ -1,12 +1,10 @@
 package com.rabbit.rabbitmqconsumer.service;
 
-//import com.rabbit.rabbitmqconsumer.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
 import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.stereotype.Component;
 
 
